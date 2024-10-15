4463802eeb4cc6fd40d9f07fc3471300d16bb205
Vithushan Logeswaran <lvithu123@outlook.com>
2024-10-15 19:41:33 +0530

    Initial Commit

---
+++ simpleApp/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  simpleApp
+//
+//  Created by Vithushan Logan on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simpleAppTests/simpleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppTests.swift
+//  simpleAppTests
+//
+//  Created by Vithushan Logan on 2024-10-15.
+//
+
+import Testing
+@testable import simpleApp
+
+struct simpleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
